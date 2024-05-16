66592810ad1f600658f27455f77ec1aea95fd77e
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-16 12:26:27 -0400

    ajout de style a la modale  et multiple svg

--- clone-twitter/src/Components/ModalPost/ModalPost.jsx
+++ clone-twitter/src/Components/ModalPost/ModalPost.jsx
@@ -1,28 +1,53 @@
 import './ModalPost.css'
 import close from '../../assets/close.svg'
+import japan from '../../assets/japan.jpg'
+import earth from '../../assets/earth.svg'
+import ModalIcoPost from '../../Layouts/ModalIcoPost/ModalIcoPost'
 
 
-export default function ModalPost({closemodal}) {
+export default function ModalPost({ closemodal }) {
   return (
-    <div 
-    onClick={closemodal}
-    className="backgroundModal">
+    <div
+      onClick={closemodal}
+      className="backgroundModal">
       <div
-      onClick={e => e.stopPropagation}
-      className='ModalCard'
+        onClick={e => e.stopPropagation()}
+        className='ModalCard'
       >
         <div className='ModalCardTop'>
           <button
-              onClick={closemodal}
+            onClick={closemodal}
           >
             <img src={close} alt="close modale" />
           </button>
           <p>Drafts</p>
-          
+        </div>
+        <div
 
+          className="ModalCardText">
+          <div className="modalImg">
+            <img src={japan} alt="" />
+
+          </div>
+          <textarea
+            placeholder="What's happening?"
+            className="postTextarea"
+          />
+        </div>
+        <div className="modalCardbot">
+          <div className="topbox">
+            <p>
+              <img src={earth} alt="" />
+              <span>Everyone can reply</span>
+            </p>
+          </div>
+          <div className='botbox'>
+            <ModalIcoPost/>
+          </div>
         </div>
       </div>
-  
+
+
     </div>
   )
 }
---
+++ clone-twitter/src/Layouts/ModalIcoPost/ModalIcoPost.jsx
@@ -0,0 +1,12 @@
+import './ModalIcoPost.css'
+import picture from '../../assets/picture.svg'
+
+export default function ModalIcoPost() {
+  return (
+    <div>
+        <img src={picture} alt="" />
+        peddd
+        dddddddd
+    </div>
+  )
+}

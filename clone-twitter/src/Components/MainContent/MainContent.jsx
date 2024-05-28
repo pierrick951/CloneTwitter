22705a68facce24d496e9b84996ac19bb4cd0736
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-28 10:01:05 -0400

    debut du contenu de main content et ajout de la premiere partie du header

--- clone-twitter/src/Components/MainContent/MainContent.jsx
+++ clone-twitter/src/Components/MainContent/MainContent.jsx
@@ -1,8 +1,22 @@
 import './MainContent.css'
+import japan from '../../assets/japan.jpg'  
+import xlogo from '../../assets/xlogo.png'  
+import cog from '../../assets/cog.svg'  
 
 
 export default function Main() {
   return (
-    <div className='MainContainer'>Main</div>
+    <div className='MainContainer'>
+      <div className="GlassyBox">
+        <div className='topGlassy'>
+          <img 
+       
+          src={japan} alt="profil picture" />
+          <img src={xlogo} alt="x logo" />
+          <img src={cog} alt="options" />
+        </div>
+ 
+      </div>
+    </div>
   )
 }
